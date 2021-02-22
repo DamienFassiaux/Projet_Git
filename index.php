fef7a347329743e7bdb7e9571b91abe25e36e346
Damien Fassiaux <damienfassiaux@gmail.com>
2021-02-22 14:32:11 +0100

    ajout caroussel page index

--- index.php
+++ index.php
@@ -29,7 +29,25 @@
 
         <h1 class="display-4 text-center my-5">Mon projet Git</h1>
 
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                  <img src="https://picsum.photos/id/237/200/300" class="d-block w-100" alt="">
+              </div>
+              <div class="carousel-item">
+                <img src="https://picsum.photos/id/238/200/300" class="d-block w-100" alt="">
+              </div>
+              <div class="carousel-item">
+                <img src="https://picsum.photos/id/239/200/300" class="d-block w-100" alt="">
+              </div>
+            </div>
+          </div>
 
+      <section></section>
+      <section></section>
+      <section></section>
+
+      <article></article>
 
     </div>
 
